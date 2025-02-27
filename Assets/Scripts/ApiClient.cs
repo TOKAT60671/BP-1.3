0c4e6dd2542c74c0f661333228d66030c49e409d
TOKAT60671 <tomasramerman@gmail.com>
2025-02-27 09:48:18 +0100

    aanmaken

---
+++ Assets/Scripts/ApiClient.cs
@@ -0,0 +1,72 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class ApiClient : MonoBehaviour
+{
+   public async void Register()
+    {
+        var request = new PostRegisterRequestDto()
+        {
+            email = "naam321432432134@test.nl",
+            password = "33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!"
+        };
+
+       var jsondata = JsonUtility.ToJson(request);
+        Debug.Log(jsondata);
+        await PerformApiCall("https://avansict123456.azurewebsites.net/account/register", "POST", jsondata);
+    }
+
+    public async void Login()
+    {
+        var request = new PostRegisterRequestDto()
+        {
+            email = "orm123@test.nl",
+            password = "33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!33nW@chtWoord!"
+        };
+
+        var jsondata = JsonUtility.ToJson(request);
+        Debug.Log(jsondata);
+        var response = await PerformApiCall("https://avansict123456.azurewebsites.net/account/register", "POST", jsondata);
+        Debug.Log(response);
+        var ResponseDto = JsonUtility.FromJson<PostLoginRequestDto>(response);
+    }
+
+
+
+    private async Task<string> PerformApiCall(string url, string method, string jsonData = null, string token = null)
+    {
+        using (UnityWebRequest request = new UnityWebRequest(url, method))
+        {
+            if (!string.IsNullOrEmpty(jsonData))
+            {
+                byte[] jsonToSend = Encoding.UTF8.GetBytes(jsonData);
+                request.uploadHandler = new UploadHandlerRaw(jsonToSend);
+            }
+
+            request.downloadHandler = new DownloadHandlerBuffer();
+            request.SetRequestHeader("Content-Type", "application/json");
+
+            if (!string.IsNullOrEmpty(token))
+            {
+                request.SetRequestHeader("Authorization", "Bearer " + token);
+            }
+
+            request.SendWebRequest();
+            if (request.result == UnityWebRequest.Result.Success)
+            {
+                Debug.Log("API-aanroep is successvol: " + request.downloadHandler.text);
+
+                return request.downloadHandler.text;
+            }
+            else
+            {
+                Debug.Log("Fout bij API-aanroep: " + request.error);
+                return null;
+            }
+        }
+    }
+}
---
+++ Assets/Scripts/PostRegisterRequestDto.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PostRegisterRequestDto : MonoBehaviour
+{
+    public string email;
+    public string password;
+
+}
